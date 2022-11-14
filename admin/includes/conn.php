c2a0f51b159dcd090cb301f4e8b29c3060aca62c
lusitechmall <hamdanialhassangandi2020@gmail.com>
2022-11-14 04:06:39 +0000

    admin

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$conn = new mysqli('database-1.cq9eihbyetk7.us-east-1.rds.amazonaws.com', 'admin', 'Mysql#$%2020', 'votesystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
